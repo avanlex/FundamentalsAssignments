796171aca27c143bb32e25a638f4d5434523c69a
avanlex <nekolex@gmail.com>
2020-12-03 03:29:05 +0300

    Dev fragments (#3)
    * created:
    - item_layout
    - details fragment
    - list fragment
    
    * Clean up
    
    * Fixed Review Notes

---
+++ app/src/main/java/com/github/avanlex/fundamentalsassignments/FragmentMoviesDetails.kt
@@ -0,0 +1,25 @@
+package com.github.avanlex.fundamentalsassignments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class  FragmentMoviesDetails : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+        view.findViewById<TextView>(R.id.text_back)
+            .setOnClickListener {
+                fragmentManager?.popBackStack()
+            }
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/github/avanlex/fundamentalsassignments/FragmentMoviesList.kt
@@ -0,0 +1,29 @@
+package com.github.avanlex.fundamentalsassignments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.imageview.ShapeableImageView
+
+class FragmentMoviesList : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View  {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        view.findViewById<ShapeableImageView>(R.id.shImageMoviePoster)
+            .setOnClickListener {
+                fragmentManager!!.beginTransaction()
+                    .replace(R.id.main_activity, FragmentMoviesDetails())
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        return view
+    }
+}
