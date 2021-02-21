1be7477f0c60a770fd223abcdf196319dfbbd4ea
avanlex <nekolex@gmail.com>
2021-02-21 23:46:05 +0300

    Optimize imports

--- app/src/main/java/com/github/avanlex/fundamentalsassignments/movieList/data/Genre.kt
+++ app/src/main/java/com/github/avanlex/fundamentalsassignments/movieList/data/Genre.kt
@@ -2,7 +2,6 @@ package com.github.avanlex.fundamentalsassignments.movieList.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Serializable
 
 @Parcelize
 data class Genre(
